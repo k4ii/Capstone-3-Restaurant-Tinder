cf3afa08cecd28d093e386c458def467869396f2
Matthew Min <matthew.min96@gmail.com>
2023-06-23 09:27:15 -0400

    [FIX] replace schema in create.sh from schema.sql => new_schema.sql

--- java/database/create.sh
+++ java/database/create.sh
@@ -3,6 +3,6 @@ BASEDIR=$(dirname $0)
 DATABASE=final_capstone
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/new_schema.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/restaurants.sql"
